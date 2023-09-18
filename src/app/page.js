ba341657577838a2c76306168defdda2ee152bd4
Gerson <salmeron.gerson@gmail.com>
2023-09-17 22:41:31 -0400

    Flask and python requirements added in preparation of the MLB Stats API and Valo API

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,16 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+        Get started by editing&nbsp;
+          <Link href="/api/python">
+            <code className="font-mono font-bold">api/index.py</code>
+          </Link>
         </p>
         <div>
           <a
