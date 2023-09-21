173bf1f0b81c338a3d22e2b9b400310e302abe00
Gerson <salmeron.gerson@gmail.com>
2023-09-21 18:38:28 -0400

    Inital homepage Sports Grid work with images.

--- src/app/_components/home/sportsgrid.js
+++ src/app/_components/home/sportsgrid.js
@@ -1,14 +1,19 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 const SportsGrid = () => {
   return (
     <div className="sports-grid">
       <div className="sports-grid__item">
-        <p>
+          <Image
+            src="/images/baseball.svg"
+            width={200}
+            height={100}
+            alt="Baseball"
+          ></Image>
           <Link href="/baseball">
             Baseball
           </Link>
-        </p>
       </div>
     </div>
   );
