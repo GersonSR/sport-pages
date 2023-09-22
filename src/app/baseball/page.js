d76161cb58423da18fadbc9921c8b8e181f4e10c
Gerson <salmeron.gerson@gmail.com>
2023-09-22 19:35:25 -0400

    Baseball leagues formatting with some styling.

--- src/app/baseball/page.js
+++ src/app/baseball/page.js
@@ -1,15 +1,36 @@
-import Link from 'next/link';
+import Link from "next/link";
+import Image from "next/image";
+
+import styles from "./page.module.css";
 
 const SportPage = () => {
   return (
-    <div>
-      <h1>League Page</h1>
-      <p>Choose which league to view:</p>
-      <ul>
-        <li><Link href="/baseball/MLB">MLB</Link></li>
+    <main className={styles.main}>
+      <h1 className={styles.header}>
+        <span> Baseball Leagues </span>
+        <Image
+          src="/images/baseball.svg"
+          alt="Baseball"
+          width={200}
+          height={100}
+        />
+      </h1>
+      <ul className={styles.leagues}>
+        <p>Choose which league to view:</p>
+        <li className={styles.league}>
+          <Link href="/baseball/MLB">
+            <Image 
+              src="/images/baseball/mlb/mlblogo.svg"
+              alt="MLB Logo"
+              width={128}
+              height={72}
+            ></Image>
+            <span>MLB</span>
+            </Link>
+        </li>
       </ul>
-    </div>
+    </main>
   );
-}
+};
 
 export default SportPage;
