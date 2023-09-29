8881fd0e8d8b4615ac2c27e5344c6c687ad85485
Gerson <salmeron.gerson@gmail.com>
2023-09-29 16:27:09 -0400

    parsing standings data.

--- src/app/baseball/MLB/_components/divisionstandings.js
+++ src/app/baseball/MLB/_components/divisionstandings.js
@@ -1,12 +1,37 @@
 import styles from "./divisionstandings.module.css";
 
-const DivisionStandings = ({division}) => {
-  console.log(division);
-  return(
+const DivisionStandings = ({ division }) => {
+  let calculatePCT = (wins, losses) => {
+    return Number(wins / (wins + losses)).toFixed(3);
+  };
+
+  return (
     <div className={styles["division"]}>
-      {division.div_name}
+      <span>{division.div_name}</span>
+      <table className="teams-table">
+        <thead>
+          <tr>
+            <th>Teams</th>
+            <th>W</th>
+            <th>L</th>
+            <th>PCT</th>
+            <th>GB</th>
+          </tr>
+        </thead>
+        <tbody>
+          {division.teams.map((team) => (
+            <tr key={team.team_id}>
+              <td>{team.name}</td>
+              <td>{team.w}</td>
+              <td>{team.l}</td>
+              <td>{calculatePCT(team.w, team.l)}</td>
+              <td>{team.gb}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default DivisionStandings;
