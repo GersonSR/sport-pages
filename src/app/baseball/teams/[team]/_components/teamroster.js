3a6dcb1c78fba2433f7f325b3dbc2340934b4ebe
Gerson <salmeron.gerson@gmail.com>
2023-10-01 21:01:56 -0400

    team roster init.

--- src/app/baseball/teams/[team]/_components/teamroster.js
+++ src/app/baseball/teams/[team]/_components/teamroster.js
@@ -1,3 +1,9 @@
+"use client" //Will be using state for this component
+
+import styles from "./teamroster.module.css";
+
+import { useState } from "react";
+
 const TeamRoster = ({ team }) => {
   return (
     <div>{team.name} Team Roster</div>
