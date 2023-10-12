c8620df153781cb901e71dc3e1a5a9c0ac7caeff
Gerson <salmeron.gerson@gmail.com>
2023-10-12 19:59:42 -0400

    further player info fields and conditionals incase of missing data.

--- src/app/baseball/player/[player]/_components/playerinfo.js
+++ src/app/baseball/player/[player]/_components/playerinfo.js
@@ -10,36 +10,98 @@ const PlayerInfo = ({ player }) => {
       </div>
       <div className={styles["player-rundown"]}>
         <div className={styles["player-rundown_item"]}>
-          <div className={styles["player-rundown_item__name"]}>Player Full Name:</div>
-          <div className={styles["player-rundown_item__value"]}>{playerInfo.fullFMLName}</div>
+          <div className={styles["player-rundown_item__name"]}>
+            Player Full Name:
+          </div>
+          <div className={styles["player-rundown_item__value"]}>
+            {playerInfo.fullFMLName ? playerInfo.fullFMLName : "N/A"}
+          </div>
         </div>
         <div className={styles["player-rundown_item"]}>
           <div className={styles["player-rundown_item__name"]}>Active:</div>
-          <div className={styles["player-rundown_item__value"]}>{playerInfo.active ? "Yes" : "No"}</div>
+          <div className={styles["player-rundown_item__value"]}>
+            {playerInfo.active ? "Yes" : "No"}
+          </div>
         </div>
         <div className={styles["player-rundown_item"]}>
           <div className={styles["player-rundown_item__name"]}>Birth Date:</div>
-          <div className={styles["player-rundown_item__value"]}>{playerInfo.birthDate}</div>
+          <div className={styles["player-rundown_item__value"]}>
+            {playerInfo.birthDate ? playerInfo.birthDate : "N/A"}
+          </div>
         </div>
         <div className={styles["player-rundown_item"]}>
           <div className={styles["player-rundown_item__name"]}>Hometown:</div>
-          <div className={styles["player-rundown_item__value"]}>{`${playerInfo.birthCity}, ${playerInfo.birthStateProvince}, ${playerInfo.birthCountry}`}</div>
+          <div className={styles["player-rundown_item__value"]}>
+            {playerInfo.birthCity &&
+            playerInfo.birthStateProvince &&
+            playerInfo.birthCountry
+              ? `${playerInfo.birthCity}, ${playerInfo.birthStateProvince}, ${playerInfo.birthCountry}`
+              : "N/A"}
+          </div>
         </div>
         <div className={styles["player-rundown_item"]}>
           <div className={styles["player-rundown_item__name"]}>Age:</div>
-          <div className={styles["player-rundown_item__value"]}>{playerInfo.currentAge}</div>
+          <div className={styles["player-rundown_item__value"]}>
+            {playerInfo.currentAge ? playerInfo.currentAge : "N/A"}
+          </div>
         </div>
         <div className={styles["player-rundown_item"]}>
           <div className={styles["player-rundown_item__name"]}>Height:</div>
-          <div className={styles["player-rundown_item__value"]}>{playerInfo.height}</div>
+          <div className={styles["player-rundown_item__value"]}>
+            {playerInfo.height ? playerInfo.height : "N/A"}
+          </div>
         </div>
         <div className={styles["player-rundown_item"]}>
           <div className={styles["player-rundown_item__name"]}>Weight:</div>
-          <div className={styles["player-rundown_item__value"]}>{playerInfo.weight}</div>
+          <div className={styles["player-rundown_item__value"]}>
+            {playerInfo.weight ? playerInfo.weight : "N/A"}
+          </div>
         </div>
         <div className={styles["player-rundown_item"]}>
           <div className={styles["player-rundown_item__name"]}>Draft Year:</div>
-          <div className={styles["player-rundown_item__value"]}>{playerInfo.draftYear}</div>
+          <div className={styles["player-rundown_item__value"]}>
+            {playerInfo.draftYear ? playerInfo.draftYear : "N/A"}
+          </div>
+        </div>
+        <div className={styles["player-rundown_item"]}>
+          <div className={styles["player-rundown_item__name"]}>
+            Major League Debut:
+          </div>
+          <div className={styles["player-rundown_item__value"]}>
+            {playerInfo.mlbDebutDate ? playerInfo.mlbDebutDate : "N/A"}
+          </div>
+        </div>
+        <div className={styles["player-rundown_item"]}>
+          <div className={styles["player-rundown_item__name"]}>
+            Jersey Number:
+          </div>
+          <div className={styles["player-rundown_item__value"]}>
+            {playerInfo.primaryNumber ? playerInfo.primaryNumber : "N/A"}
+          </div>
+        </div>
+        <div className={styles["player-rundown_item"]}>
+          <div className={styles["player-rundown_item__name"]}>
+            Primary Position:
+          </div>
+          <div className={styles["player-rundown_item__value"]}>
+            {playerInfo.primaryPosition.name && playerInfo.primaryPosition.abbreviation ? ` ${playerInfo.primaryPosition.name} (${playerInfo.primaryPosition.abbreviation})` : "N/A"}
+          </div>
+        </div>
+        <div className={styles["player-rundown_item"]}>
+          <div className={styles["player-rundown_item__name"]}>
+            Throwing Hand:
+          </div>
+          <div className={styles["player-rundown_item__value"]}>
+            {playerInfo.pitchHand.description ? playerInfo.pitchHand.description : "N/A"}
+          </div>
+        </div>
+        <div className={styles["player-rundown_item"]}>
+          <div className={styles["player-rundown_item__name"]}>
+            Batting Side:
+          </div>
+          <div className={styles["player-rundown_item__value"]}>
+            {playerInfo.batSide.description ? playerInfo.batSide.description : "N/A"}
+          </div>
         </div>
       </div>
     </div>
