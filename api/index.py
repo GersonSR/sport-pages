552038962ffaa25f122f82bba766e12892ff31ee
Gerson <salmeron.gerson@gmail.com>
2023-10-18 13:36:51 -0400

    Updated Flask CORS

--- api/index.py
+++ api/index.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 
-CORS(app)
+CORS(app, origins=["http://localhost:3000"])
 
 @app.route("/api/mlb/schedule/<date>")
 def mlb_schedule_single(date):
