64aa39b101573872cfec6ef7d6092d5e3abbddcc
Gerson <salmeron.gerson@gmail.com>
2023-10-22 18:01:53 -0400

    Game context within the layout file created.

---
+++ src/app/baseball/games/[game]/layout.js
@@ -0,0 +1,42 @@
+"use client"
+
+import { useEffect, useState, createContext } from 'react';
+
+
+export const GameContext = createContext(null);
+
+const GameLayout = ({ children, params }) => {
+    const [game, setGame] = useState(null);
+    const [loading, setIsLoaded] = useState(false);
+    const [error, setError] = useState(null);
+
+    const getGame = async () => {
+        setIsLoaded(false);
+        try {
+            const response = await fetch(`/api/mlb/game/${params.game}`);
+            if (!response.ok) {
+                throw new Error("Game could not be retrieved!");
+            }
+            else {
+                const data = await response.json();
+                setGame(data);
+            }
+        } catch (error) {
+            setError(error.message);
+        }
+        setIsLoaded(true);
+    }
+
+    useEffect(() => {
+        getGame();
+    }, []);
+
+
+    return (
+        <GameContext.Provider value={{ game, loading, error }}>
+            { children }
+        </GameContext.Provider>
+    );
+};
+
+export default GameLayout;
--- src/app/baseball/games/[game]/page.js
+++ src/app/baseball/games/[game]/page.js
@@ -1,4 +1,20 @@
+"use client"
+import { useContext } from 'react';
+import { GameContext } from './layout';
+
 const GamePage = ({ params }) => {
+    const { game, loading, error } = useContext(GameContext);
+
+    if (!loading) {
+        return <div>Loading...</div>
+    } else if (error) {
+        return <div>{error}</div>
+    } else if (!game) {
+        return <div>Game not found!</div>
+    } else {
+        console.log(game);
+    }
+
     return (
         <div>
             <h1>Game Page</h1>
