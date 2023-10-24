0648c16e208284acc56d3ffae843c79e32ce8283
Gerson <salmeron.gerson@gmail.com>
2023-10-24 15:53:04 -0400

    Changed margin location.

--- src/app/baseball/games/[game]/_components/scoreboard/gamescore.js
+++ src/app/baseball/games/[game]/_components/scoreboard/gamescore.js
@@ -1,7 +1,24 @@
+import { GameContext } from "../../layout";
+import { useContext } from "react";
+
+import styles from "./gamescore.module.css";
+
 const GameScore = () => {
+    const { game, loading, error } = useContext(GameContext);
+
+
+
     return (
-        <div className="game-score">
-            Away (score) vs Home (score)
+        <div className={styles["game-score"]}>
+            <div>
+                <div>Away Team</div>
+                <div>0</div>
+            </div>
+            <div>-</div>
+            <div>
+                <div>Home Team</div>
+                <div>0</div>
+            </div>
         </div>
     );
 };
