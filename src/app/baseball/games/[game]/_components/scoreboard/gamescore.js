6f98000a38ac6b6881897eab2267811935490983
Gerson <salmeron.gerson@gmail.com>
2023-10-24 16:48:30 -0400

    Game score work.

--- src/app/baseball/games/[game]/_components/scoreboard/gamescore.js
+++ src/app/baseball/games/[game]/_components/scoreboard/gamescore.js
@@ -6,18 +6,24 @@ import styles from "./gamescore.module.css";
 const GameScore = () => {
     const { game, loading, error } = useContext(GameContext);
 
+    // Retrieve team info
+    const awayTeam = game["teams"]["away"];
+    const homeTeam = game["teams"]["home"];
 
+    // Retrieve team scores
+    const awayScore = game["linescore"]["teams"]["away"]["runs"];
+    const homeScore = game["linescore"]["teams"]["home"]["runs"];
 
     return (
         <div className={styles["game-score"]}>
             <div>
                 <div>Away Team</div>
-                <div>0</div>
+                <div>{awayScore}</div>
             </div>
-            <div>-</div>
+            <div> at </div>
             <div>
                 <div>Home Team</div>
-                <div>0</div>
+                <div>{homeScore}</div>
             </div>
         </div>
     );
