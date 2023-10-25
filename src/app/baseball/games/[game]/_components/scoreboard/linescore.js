f1c31d1e069931110b38dfd64e9118e348a5ced9
Gerson <salmeron.gerson@gmail.com>
2023-10-25 16:15:55 -0400

    Linescore starts.

--- src/app/baseball/games/[game]/_components/scoreboard/linescore.js
+++ src/app/baseball/games/[game]/_components/scoreboard/linescore.js
@@ -1,7 +1,14 @@
+import { GameContext } from "../../layout";
+import { useContext } from "react";
+
+import styles from "./linescore.module.css"
+
 const LineScore = () => {
+    const { game, loading, error } = useContext(GameContext);
+
     return (
         <div className="linescore">
-            1st Inning, 2nd Inning, etc etc 
+            1st Inning, 2nd Inning, etc etc. 1st | 2nd |  R | H | E
         </div>
     );
 };
