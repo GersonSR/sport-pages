20670d9a3b572b06efea7012334d9c2ba603d73a
Gerson <salmeron.gerson@gmail.com>
2023-10-30 12:16:39 -0400

    Initial notes containers.

--- src/app/baseball/games/[game]/_components/boxscore/boxtable.js
+++ src/app/baseball/games/[game]/_components/boxscore/boxtable.js
@@ -1,37 +1,58 @@
 
 
-import { useContext } from 'react';
-import { GameContext } from '../../layout'; 
+import { Fragment, useContext } from 'react';
+import { GameContext } from '../../layout';
 
 import styles from './boxtable.module.css';
 import StatsTable from './boxstatstable';
+import StatsTableNotes from './boxtablenotes';
 
 const BoxTable = ({ tableType, tableOrder }) => {
     const { game, loading, error } = useContext(GameContext);
     let awayPlayerInfo = [];
     let homePlayerInfo = [];
+
     let homeTotals = {};
     let awayTotals = {};
+
+    let homeNotes = [];
+    let awayNotes = [];
 
     if (tableType === "Batters") {
         awayPlayerInfo = game["awayBatters"];
         awayTotals = game["awayBattingTotals"];
+        awayNotes = game["awayBattingNotes"];
+
         homePlayerInfo = game["homeBatters"];
         homeTotals = game["homeBattingTotals"];
+        homeNotes = game["homeBattingNotes"];
     } else {
         awayPlayerInfo = game["awayPitchers"];
         awayTotals = game["awayPitchingTotals"];
+        awayNotes = game["awayPitchingNotes"];
+
         homePlayerInfo = game["homePitchers"];
         homeTotals = game["homePitchingTotals"];
+        homeNotes = game["homePitchingNotes"];
     }
 
     return (
-        <div className={styles["boxstats-tables-container"]}>
-            <div className={styles["boxstats-team-container"]}>
-                <StatsTable stats={awayPlayerInfo} order={tableOrder} totals={awayTotals}/>
+        <div className={styles["box-stats-container"]}>
+            <div className={styles["boxstats-tables-container"]}>
+                <div className={styles["boxstats-team-container"]}>
+                    <StatsTable stats={awayPlayerInfo} order={tableOrder} totals={awayTotals} />
+                </div>
+                <div className={styles["boxstats-team-container"]}>
+                    <StatsTable stats={homePlayerInfo} order={tableOrder} totals={homeTotals} />
+                </div>
             </div>
-            <div className={styles["boxstats-team-container"]}>
-                <StatsTable stats={homePlayerInfo} order={tableOrder} totals={homeTotals}/>
+            <div className={styles["boxstats-tables-container"]}>
+                <div className={styles["boxstats-team-container"]}>
+                    <StatsTableNotes notes={awayNotes} />
+                </div>
+                <div className={styles["boxstats-team-container"]}>
+                    <StatsTableNotes notes={homeNotes} />
+                </div>
             </div>
         </div>
     );
---
+++ src/app/baseball/games/[game]/_components/boxscore/boxtablenotes.js
@@ -0,0 +1,10 @@
+
+const StatsTableNotes = ({ notes }) => {
+    return (
+        <div className="stats-table-notes">
+            Notes here!
+        </div>
+    );
+}
+
+export default StatsTableNotes;
