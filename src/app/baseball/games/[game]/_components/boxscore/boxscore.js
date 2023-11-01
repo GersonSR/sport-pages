e2dda24bed94804484d2b14b739bd004f5abadb3
Gerson <salmeron.gerson@gmail.com>
2023-11-01 13:44:23 -0400

    Pitchers added and some additional game table formatting also added.

--- src/app/baseball/games/[game]/_components/boxscore/boxscore.js
+++ src/app/baseball/games/[game]/_components/boxscore/boxscore.js
@@ -4,12 +4,13 @@ import BoxTable from './boxtable';
 const BoxScore = () => {
 
     const battersTableFields = ["namefield", "ab", "r", "h", "rbi", "bb", "k", "lob", "avg", "ops"];
-
+    const pitcherTableFields = ["namefield", "ip", "h", "r", "er", "bb", "k", "hr", "era"]
 
     return (
         <div className={styles["boxscore-container"]}>
             <h2>Box Score</h2>
             <BoxTable tableType={"Batters"} tableOrder={battersTableFields}/>
+            <BoxTable tableType={"Pitchers"} tableOrder={pitcherTableFields}/>
         </div>
     );
 };
