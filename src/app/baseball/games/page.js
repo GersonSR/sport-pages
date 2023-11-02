25de23e7a73025d9a5f5fcc86d433df921bf33d3
Gerson <salmeron.gerson@gmail.com>
2023-11-02 12:42:41 -0400

    Refactoring of score related components for more easy of use.

--- src/app/baseball/games/page.js
+++ src/app/baseball/games/page.js
@@ -3,7 +3,9 @@ import styles from './page.module.css';
 
 const GamesPage = () => {
   return (
-    <main className={styles["games-main-container"]}> This will feature the schedule for leagues eventually! </main>
+    <main className={styles["games-main-container"]}> 
+      <h3>This will feature the schedule for leagues eventually!</h3> 
+    </main>
   );
 }
 
