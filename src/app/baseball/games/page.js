a2dd008907267d99ed450bf136a5f669802ec5f9
Gerson <salmeron.gerson@gmail.com>
2023-11-03 12:03:49 -0400

    Date picker for games added and leagues array.

--- src/app/baseball/games/page.js
+++ src/app/baseball/games/page.js
@@ -1,11 +1,35 @@
+"use client"
 
+import { useEffect, useState } from 'react';
+
+import ReactDatePicker from 'react-datepicker';
 import ScoreTable from '../_components/scoretable/scoretable';
+
 import styles from './page.module.css';
+import "react-datepicker/dist/react-datepicker.css";
 
 const GamesPage = () => {
+  let timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [formattedDate, setFormattedDate] = useState(new Date().toLocaleDateString(navigator.language, {timeZone: timeZone, year: 'numeric', month: 'long', day: 'numeric'}));
+  const maxDate = new Date();
+
+  const leagues = ["MLB"];
+
+  const handleDateChange = (date) => {
+    setStartDate(date);
+  }
+
+  useEffect(() => {
+    let formattedDate = startDate.toLocaleDateString(navigator.language, {timeZone: timeZone, year: 'numeric', month: 'long', day: 'numeric'});
+    setFormattedDate(formattedDate);
+  }, [startDate]);
+
   return (
     <main className={styles["games-main-container"]}> 
-      <h3>Baseball Games for {"Date Here"}</h3>
+      <h3>Baseball Games for {formattedDate}</h3>
+      <ReactDatePicker selected={startDate} onChange={handleDateChange} maxDate={maxDate} />
       <ScoreTable league="MLB" date="06-06-2016" verbosity="expanded"/> 
     </main>
   );
