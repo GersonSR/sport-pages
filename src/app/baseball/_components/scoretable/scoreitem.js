c125367ffece15757b3ead00e3a3168c0e86ac32
Gerson <salmeron.gerson@gmail.com>
2023-11-07 10:43:06 -0500

    Styling changes more uniform in score items.

--- src/app/baseball/_components/scoretable/scoreitem.js
+++ src/app/baseball/_components/scoretable/scoreitem.js
@@ -6,9 +6,7 @@ import Link from "next/link";
 
 const ScoreItem = (props) => {
   const score = props.score;
-
   let displayScores = false;
-
   const gameTime = new Date(score["game_datetime"]);
 
   let status = score['status'];
@@ -21,13 +19,13 @@ const ScoreItem = (props) => {
   // const awayRecord = fetch("/api/mlb/record/" + score["away_id"]);
   return (
     <div className={styles["score-item"]}>
-      <Link href={`/baseball/games/${score.game_id}`} className={styles["status"]}>{status}</Link>
-      <div className={styles.score}>
-        <Link href={`/baseball/teams/${score.away_id}`} className={styles["away-name"]}>{score["away_name"]}</Link>
+      <Link href={`/baseball/games/${score["game_id"]}`} className={styles["status"]}>{status}</Link>
+      <div className={styles["score"]}>
+        <Link href={`/baseball/teams/${score["away_id"]}`} className={styles["away-name"]}>{score["away_name"]}</Link>
         <div className={styles["away-score"]}>{score["away_score"]}</div>
       </div>
-      <div className={styles.score}>
-        <Link href={`/baseball/teams/${score.home_id}`} className={styles["home-name"]}>{score["home_name"]}</Link>
+      <div className={styles["score"]}>
+        <Link href={`/baseball/teams/${score["home_id"]}`} className={styles["home-name"]}>{score["home_name"]}</Link>
         <div className={styles["home-score"]}>{score["home_score"]}</div>
       </div>
     </div>
