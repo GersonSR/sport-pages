3f5d7afed260b88094588042bca614300946ac70
Gerson <salmeron.gerson@gmail.com>
2023-11-08 12:23:44 -0500

    Initial creation of seperate verbosed score items.

--- src/app/baseball/_components/scoretable/scoreitem.js
+++ src/app/baseball/_components/scoretable/scoreitem.js
@@ -1,35 +1,19 @@
-//Currently will be hardcoded for baseball/MLB. Will need to be updated to be dynamic for all sports/leagues.
+import { Fragment } from "react";
+import SimpleScoreItem from "./scoreitems/simplescoreitem"
 
-import { useEffect } from "react";
-import styles from "./scoreitem.module.css";
-import Link from "next/link";
+const ScoreItem = ({ score, verbosity }) => {
+    let content;
 
-const ScoreItem = (props) => {
-  const score = props.score;
-  let displayScores = false;
-  const gameTime = new Date(score["game_datetime"]);
-
-  let status = score['status'];
-
-  if (status === "In Progress") { 
-    displayScores = true;
-    status = score['inning_state'] + " " + score['current_inning'];
-  }
-  
-  // const awayRecord = fetch("/api/mlb/record/" + score["away_id"]);
-  return (
-    <div className={styles["score-item"]}>
-      <Link href={`/baseball/games/${score["game_id"]}`} className={styles["status"]}>{status}</Link>
-      <div className={styles["score"]}>
-        <Link href={`/baseball/teams/${score["away_id"]}`} className={styles["away-name"]}>{score["away_name"]}</Link>
-        <div className={styles["away-score"]}>{score["away_score"]}</div>
-      </div>
-      <div className={styles["score"]}>
-        <Link href={`/baseball/teams/${score["home_id"]}`} className={styles["home-name"]}>{score["home_name"]}</Link>
-        <div className={styles["home-score"]}>{score["home_score"]}</div>
-      </div>
-    </div>
-  );
+    if (verbosity === "simple") {
+        content = <SimpleScoreItem score={score} verbosity={verbosity}></SimpleScoreItem>
+    } else if (verbosity === "expanded") { 
+        content = <SimpleScoreItem score={score} verbosity={verbosity}></SimpleScoreItem>
+    }
+    return (
+        <Fragment>
+            {content}
+        </Fragment>
+    );
 };
 
 export default ScoreItem;
